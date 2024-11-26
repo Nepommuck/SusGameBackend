257aedd7e5511861ef4af15ca62a036666fb88d7
aszarata <aszarata@student.agh.edu.pl>
2024-11-26 23:17:34 +0100

    Packet route in dto fixed

--- src/main/kotlin/edu/agh/susgame/back/domain/net/node/Host.kt
+++ src/main/kotlin/edu/agh/susgame/back/domain/net/node/Host.kt
@@ -87,7 +87,10 @@ class Host(
         val nonNullRoute = route ?: emptyList()
         return HostDTO(
             id = index,
-            packetRoute = nonNullRoute.map { it.index }
+            packetRoute = buildList {
+                firstNode?.let { add(it.index) }
+                addAll(nonNullRoute.map { it.index })
+            }
         )
     }
 
